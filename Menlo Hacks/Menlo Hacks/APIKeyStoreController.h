cfd68fbbf845762c0764ff2b89f6c5986473243b
jasonscharff <jasonscharff@gmail.com>
2015-12-29 12:46:31 -0800

    Configure Parse.

---
+++ Menlo Hacks/Menlo Hacks/APIKeyStoreController.h
@@ -0,0 +1,18 @@
+//
+//  APIKeyStoreController.h
+//  Menlo Hacks
+//
+//  Created by Jason Scharff on 12/29/15.
+//  Copyright © 2015 MenloHacks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface APIKeyStoreController : NSObject
+
++ (instancetype)sharedAPIKeyStoreController;
+
+- (NSString *)getParseAppID;
+- (NSString *)getParseClientID;
+
+@end
---
+++ Menlo Hacks/Menlo Hacks/APIKeyStoreController.m
@@ -0,0 +1,48 @@
+//
+//  APIKeyStoreController.m
+//  Menlo Hacks
+//
+//  Created by Jason Scharff on 12/29/15.
+//  Copyright © 2015 MenloHacks. All rights reserved.
+//
+
+#import "APIKeyStoreController.h"
+
+@interface APIKeyStoreController()
+
+@property (nonatomic, strong) NSDictionary *apiKeyDictionary;
+
+@end
+
+@implementation APIKeyStoreController
+
++ (instancetype)sharedAPIKeyStoreController {
+  static dispatch_once_t once;
+  static APIKeyStoreController *_sharedInstance;
+  dispatch_once(&once, ^{
+    _sharedInstance = [[self alloc] init];
+  });
+  
+  return _sharedInstance;
+}
+
+- (instancetype)init {
+  self = [super init];
+  
+  NSString *filePath = [[NSBundle mainBundle] pathForResource: @"API Keys" ofType: @"plist"];
+  self.apiKeyDictionary = [NSDictionary dictionaryWithContentsOfFile:filePath];
+  
+  return self;
+}
+
+- (NSString *)getParseAppID {
+  return self.apiKeyDictionary[@"parse_application_id"];
+}
+
+- (NSString *)getParseClientID {
+  return self.apiKeyDictionary[@"parse_client_id"];
+}
+
+
+
+@end
