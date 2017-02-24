81a7d7e1b7e063e88ec20377c464ba7aba239777
Jason Scharff <jasonscharff@gmail.com>
2017-02-23 17:10:02 -1000

    Create session manager

---
+++ Menlo Hacks/Menlo Hacks/MEHHTTPSessionManager.h
@@ -0,0 +1,13 @@
+//
+//  MEHHTTPSessionManager.h
+//  Menlo Hacks
+//
+//  Created by Jason Scharff on 2/23/17.
+//  Copyright © 2017 MenloHacks. All rights reserved.
+//
+
+#import <AFNetworking/AFNetworking.h>
+
+@interface MEHHTTPSessionManager : AFHTTPSessionManager
+
+@end
---
+++ Menlo Hacks/Menlo Hacks/MEHHTTPSessionManager.m
@@ -0,0 +1,90 @@
+//
+//  MEHHTTPSessionManager.m
+//  Menlo Hacks
+//
+//  Created by Jason Scharff on 2/23/17.
+//  Copyright © 2017 MenloHacks. All rights reserved.
+//
+
+#import "MEHHTTPSessionManager.h"
+
+#import <Bolts/Bolts.h>
+
+
+static NSString * kMEHAuthorizationHeaderField = @"X-MenloHacks-Authorization";
+
+@implementation MEHHTTPSessionManager
+
++ (instancetype)sharedSessionManager {
+    static dispatch_once_t once;
+    static MEHHTTPSessionManager *_sharedInstance;
+    dispatch_once(&once, ^{
+        _sharedInstance = [[self alloc] initWithBaseURL:[NSURL URLWithString:@"https://api.menlohacks.com/"]];
+    });
+    
+    return _sharedInstance;
+}
+
+- (instancetype)initWithBaseURL:(NSURL *)url {
+    self = [super initWithBaseURL:url];
+    if(self) {
+        
+        AFJSONRequestSerializer *serializer = [AFJSONRequestSerializer serializer];
+        [serializer setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+        [serializer setValue:@"application/json" forHTTPHeaderField:@"Accept"];
+        
+        
+        [self setRequestSerializer:serializer];
+    }
+    return self;
+}
+
+
+#pragma mark error handling
+
+
+- (void)handleError : (NSError *)error {
+    //TODO: Error handling
+    //    NSLog(@"error = %@", error.localizedDescription);
+}
+
+- (void)setAuthorizationHeader {
+ //   NSString *authToken = [JNKeychain loadValueForKey:ENTKeychainAuthTokenKey];
+    [self.requestSerializer setValue:authToken forHTTPHeaderField:kMEHAuthorizationHeaderField];
+}
+
+#pragma mark networking requests with Bolts
+
+
+- (BFTask *)GET:(NSString *)URLString parameters:(id)parameters {
+    
+    BFTaskCompletionSource *completionSource = [BFTaskCompletionSource taskCompletionSource];
+    
+    [self GET:URLString parameters:parameters progress:nil
+      success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        [completionSource setResult:responseObject];
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        [self handleError:error];
+        [completionSource setError:error];
+    }];
+    
+    return completionSource.task;
+    
+}
+
+- (BFTask *)POST:(NSString *)URLString
+      parameters:(id)parameters {
+    
+    BFTaskCompletionSource *completionSource = [BFTaskCompletionSource taskCompletionSource];
+    
+    [self POST:URLString parameters:parameters progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        [completionSource setResult:responseObject];
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        [self handleError:error];
+        [completionSource setError:error];
+    }];
+    
+    return completionSource.task;
+}
+
+@end
