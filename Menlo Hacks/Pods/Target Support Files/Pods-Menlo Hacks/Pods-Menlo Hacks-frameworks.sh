d5934116712a9b3109c994f72095d541eb20526a
Jason Scharff <jasonscharff@gmail.com>
2017-03-05 11:35:22 -0800

    Install page menu

--- Menlo Hacks/Pods/Target Support Files/Pods-Menlo Hacks/Pods-Menlo Hacks-frameworks.sh
+++ Menlo Hacks/Pods/Target Support Files/Pods-Menlo Hacks/Pods-Menlo Hacks-frameworks.sh
@@ -82,3 +82,24 @@ strip_invalid_archs() {
   fi
 }
 
+
+if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/AFNetworking/AFNetworking.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Bolts/Bolts.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/FCAlertView/FCAlertView.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/JNKeychain/JNKeychain.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/PageMenu/PageMenu.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Realm/Realm.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/SocketRocket/SocketRocket.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/libPusher/Pusher.framework"
+fi
+if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/AFNetworking/AFNetworking.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Bolts/Bolts.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/FCAlertView/FCAlertView.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/JNKeychain/JNKeychain.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/PageMenu/PageMenu.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Realm/Realm.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/SocketRocket/SocketRocket.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/libPusher/Pusher.framework"
+fi
---
+++ Menlo Hacks/Pods/Target Support Files/Pods-Menlo Hacks/Pods-Menlo Hacks-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Menlo_HacksVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Menlo_HacksVersionString[];
+
