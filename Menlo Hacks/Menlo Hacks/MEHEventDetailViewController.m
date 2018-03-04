aa05f13b15ff1016026e7ba8f30b1d7582ba8e83
Jason Scharff <jasonscharff@gmail.com>
2018-03-04 17:05:37 -0500

    Fix apple being annoying

--- Menlo Hacks/Menlo Hacks/MEHEventDetailViewController.m
+++ Menlo Hacks/Menlo Hacks/MEHEventDetailViewController.m
@@ -76,7 +76,7 @@
   [AutolayoutHelper configureView:self.view
                          subViews:NSDictionaryOfVariableBindings(_mapImageViewScrollView, _timeLabel, _descriptionTextView, _descriptionLabel, _addToCalendarButton, _locationLabel)
                           constraints:@[@"H:|[_mapImageViewScrollView]|",
-                                        @"V:|[_mapImageViewScrollView]-[_descriptionLabel]-[_locationLabel]-[_timeLabel]-[_descriptionTextView]-15-[_addToCalendarButton]-75-|",
+                                        @"V:|[_mapImageViewScrollView]-[_descriptionLabel]-[_locationLabel]-[_timeLabel]-[_descriptionTextView]-15-[_addToCalendarButton]-50-|",
                                         @"X:_locationLabel.centerX == superview.centerX",
                                         @"X:_descriptionLabel.centerX == superview.centerX",
                                         @"H:|-[_descriptionLabel]-|",
